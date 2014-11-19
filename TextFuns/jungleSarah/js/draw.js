cdd007dbb2a7432426373bafa67224fa83df2244
Sarah GP <sarah.groff.palermo@gmail.com>
2014-11-18 20:30:07 -0500

    Speed fixed

--- TextFuns/jungleSarah/js/draw.js
+++ TextFuns/jungleSarah/js/draw.js
@@ -1,76 +1,110 @@
-function draw(filename, author, book) {
-  var width = window.innerWidth,
-    height = window.innerHeight;
+var DRAW = (function(){
 
-  var colors = {
-    'sinclair': 'hsla(182, 8%, 17%, 1)',
-    'kipling': 'hsla(122, 100%, 21%, 1)',
-    'both': 'hsla(233, 47%, 21%, 1)'
-  };
 
-  if (book === 'together'){
-     var svg = d3.select('#'+book)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
-  } else { 
-    var svg = d3.select('#'+book)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height/2);
+  return {
+    draw: function (filename, author, book) {
+      var width = window.innerWidth,
+        height = window.innerHeight;
+
+      var colors = {
+        'sinclair': 'hsla(182, 8%, 17%, 1)',
+        'kipling': 'hsla(122, 100%, 21%, 1)',
+        'both': 'hsla(233, 47%, 21%, 1)'
+      };
+
+      d3.select('.'+author).remove();
+
+      if (book === 'together'){
+         var svg = d3.select('#'+book)
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height)
+          .attr('class', author);
+      } else { 
+        var svg = d3.select('#'+book)
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height/2)
+          .attr('class', author);
+      }
+
+
+      d3.text(filename, function(error, text){
+
+        var n = 0,
+            data = d3.csv.parseRows(text),
+            dataView = [data[0][n]];   
+
+        svg.append('rect')
+          .data(dataView)
+          .attr('width', '100%')
+          .attr('height', '100%')
+          .attr('fill', colors[author]);
+
+        d3.select('#'+book+'-word')
+          .style('text-anchor', 'end')
+          .text(dataView)    
+
+        function changeWord() {
+
+          // Update data view
+          
+          n++;
+          n %= text.length;
+
+          dataView = [data[0][n]];
+
+
+          d3.select('#'+book+'-word')
+            .text(dataView);
+
+        }
+
+        DRAW['to'][author] = setInterval(changeWord, 250);   
+      })
+    },
+
+    'to': {
+      'kipling': undefined,
+      'sinclair': undefined,
+      'both': undefined
+    },
+
+    clearInt: function(author) {
+      clearInterval(this['to'][author]);
+      DRAW['to'][author] = undefined;
+    }
+
   }
 
 
-  d3.text(filename, function(error, text){
+})();
 
-    var n = 0,
-        data = d3.csv.parseRows(text),
-        dataView = [data[0][n]];   
-
-    svg.append('rect')
-      .data(dataView)
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .attr('fill', colors[author]);
-
-    d3.select('#'+book+'-word')
-      .style('text-anchor', 'end')
-      .text(dataView)    
-
-    function changeWord() {
-
-      // Update data view
-      
-      n++;
-      n %= text.length;
-
-      dataView = [data[0][n]];
-
-
-      d3.select('#'+book+'-word')
-        .text(dataView);
-
-    }
-
-    setInterval(changeWord, 250);   
-  })
-
-}
 
 
 $(document).ready(function(){
-  draw('data/_sinclairSorted.csv', 'sinclair', 'jungle');
-  setTimeout(draw('data/_kiplingSorted.csv', 'kipling', 'junglebook'), 150);
+  DRAW.draw('data/_sinclairSorted.csv', 'sinclair', 'jungle');
+  DRAW.draw('data/_kiplingSorted.csv', 'kipling', 'junglebook');
+
 
   $('#together-link').on('click', function(){
+    console.log(DRAW.to);
     $('#jungle, #junglebook').addClass('hidden');
+    DRAW.clearInt('kipling');
+    DRAW.clearInt('sinclair');
+
     $('#together').removeClass('hidden');
-    setTimeout(draw('data/_sharedSorted.csv', 'both', 'together'), 150);
+    DRAW.draw('data/_sharedSorted.csv', 'both', 'together');
   });
 
   $('#apart-link').on('click', function(){
+    console.log(DRAW.to);
     $('#jungle, #junglebook').removeClass('hidden');
+    DRAW.clearInt('both');
+    
     $('#together').addClass('hidden');
+    DRAW.draw('data/_sinclairSorted.csv', 'sinclair', 'jungle');
+    DRAW.draw('data/_kiplingSorted.csv', 'kipling', 'junglebook');
   });
 
   $('.legend').on('click', 'a', function(){
--- TextFuns/jungleSarah/jungle-junglebook/js/draw.js
+++ TextFuns/jungleSarah/jungle-junglebook/js/draw.js
@@ -3,24 +3,29 @@ function draw(filename, author, book) {
     height = window.innerHeight;
 
   var colors = {
-    'sinclair': 'hsla(197, 47%, 21%, 1)',
+    'sinclair': 'hsla(182, 8%, 17%, 1)',
     'kipling': 'hsla(122, 100%, 21%, 1)',
     'both': 'hsla(233, 47%, 21%, 1)'
   };
 
+  if (book === 'together'){
+     var svg = d3.select('#'+book)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+  } else { 
+    var svg = d3.select('#'+book)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height/2);
+  }
 
-  var svg = d3.select('#'+book)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height/2);
 
   d3.text(filename, function(error, text){
 
     var n = 0,
         data = d3.csv.parseRows(text),
-        dataView = [data[0][n]];
-    
-    console.log(dataView);
+        dataView = [data[0][n]];   
 
     svg.append('rect')
       .data(dataView)
@@ -39,23 +44,38 @@ function draw(filename, author, book) {
       n++;
       n %= text.length;
 
-      console.log(dataView);
-
       dataView = [data[0][n]];
 
-      // console.log(dataView);
 
       d3.select('#'+book+'-word')
         .text(dataView);
 
     }
 
-    setInterval(changeWord, 200);   
+    setInterval(changeWord, 250);   
   })
 
 }
 
+
 $(document).ready(function(){
   draw('data/_sinclairSorted.csv', 'sinclair', 'jungle');
   setTimeout(draw('data/_kiplingSorted.csv', 'kipling', 'junglebook'), 150);
+
+  $('#together-link').on('click', function(){
+    $('#jungle, #junglebook').addClass('hidden');
+    $('#together').removeClass('hidden');
+    setTimeout(draw('data/_sharedSorted.csv', 'both', 'together'), 150);
+  });
+
+  $('#apart-link').on('click', function(){
+    $('#jungle, #junglebook').removeClass('hidden');
+    $('#together').addClass('hidden');
+  });
+
+  $('.legend').on('click', 'a', function(){
+    $('.selected').toggleClass('selected');
+    $(this).addClass('selected');
+  })
+
 });
