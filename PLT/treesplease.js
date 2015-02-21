21ab0d0e559260c370574e4c7228c363e657ec31
Sarah GP <sarah.groff.palermo@gmail.com>
2015-02-21 17:29:32 -0500

    Darkness all around; light appears on the horizon

--- PLT/treesplease.js
+++ PLT/treesplease.js
@@ -1,5 +1,7 @@
 // Access functions & scope
 
+var scopeNum = -1; // to not have to subtract 1 everywhere, tho is this just going to eq assignment.thisScope?
+
 var specialForms = {
   'if': function (args) { 
     if (evaluate(args[0])){
@@ -15,34 +17,9 @@ var specialForms = {
     return specialForms.assignment(flatArgs[0], flatArgs.slice(1));
   },
 
-  'assignment': function (assign, rest, scoped) {
-    // the first expression following 'let' MUST be a binding
+  'assignment': assignment,
 
-    var variable = assign.expressions[0].expressions,
-        value = assign.expressions[1],
-        // scoped = scoped || false,
-        userScope = scopes['user'],
-        thisScope = userScope[userScope.length - 1]; 
-
-    // put identity in current scope or create new scope and add
-
-    if (scoped) {
-      thisScope[variable] = value;
-    } else {
-      userScope.push({});
-      thisScope[variable] = value; 
-    }
-      
-    // iterate through all assignments before then evaluate other expressions
-
-    if(rest.length && rest[0].operator === 'assignment'){
-      return specialForms.assignment(rest[0], rest.slice(1), true);
-    } else {
-      return moveOverArgs([], rest);
-    }
-  },
-
-  'var': lookup;
+  'var': lookup
 };
 
 var scopes = { 
@@ -72,6 +49,10 @@ var scopes = {
 // Utility functions
 
 function moveOverArgs(currentArr, arr) {
+
+  // in here have to indicate in eval call that it is in a scope if called from assignment?
+  // alternately check in eval itself?
+
   typeof arr[0] === 'object' ? currentArr.push(evaluate(arr[0])) : currentArr.push(arr[0]);
   
   var remainingArr = arr.slice(1);
@@ -107,6 +88,39 @@ function infix (operator, args) {
   return eval(args.join(operator));
 }
 
+function assignment (assign, rest, scoped) {
+
+    // increment scope number since we are now in the base user scope
+
+    scopeNum++
+    console.log('scopeNum:', scopeNum);
+
+    // the first expression following 'let' MUST be a binding
+
+    var variable = assign.expressions[0].expressions,
+        value = assign.expressions[1],
+        userScope = scopes['user'],
+        thisScope = userScope[scopeNum]; 
+
+    // put identity in current scope or create new scope and add
+
+    if (scoped) {
+      console.log('scoped!', scopes['user']);
+      thisScope[variable] = value;
+    } else { 
+      userScope.push(Object.create(Object.prototype));
+      scopes['user'][scopeNum][variable] = value;
+    }
+      
+    // iterate through all assignments before then evaluate other expressions
+
+    if(rest.length && rest[0].operator === 'assignment'){
+      return specialForms.assignment(rest[0], rest.slice(1), true);
+    } else {
+      return moveOverArgs([], rest);
+    }
+  }
+
 // Evaluation
 
 var evaluate = function(ast, scope) {
@@ -118,15 +132,21 @@ var evaluate = function(ast, scope) {
   }
 
   // Then set scope & evaluate
+  // Note that when the var operator is used, special will be called before func, assuring that it is found
 
-  var scope = scope || 'built-in',
-      special = specialForms[ast.operator],
-      func = scopes[scope][ast.operator];
+  var scope, special, func;
+
+  (scopeNum > -1) ?
+    (scope = 'user'[scopeNum]) :
+    (scope = 'built-in');
+
+  special = specialForms[ast.operator];
+  func = scopes[scope][ast.operator];
 
   console.log(ast, ast.operator, scope);
 
   if (special) {
-    return special(ast.expressions, scope);
+    return special(ast.expressions, scopeNum);
   } else if (func) {
     return func(ast.operator, ast.expressions);
   } else {
